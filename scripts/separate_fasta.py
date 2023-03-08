9f7a12b6f999981a6f066cf42a6fc7ddb4c05246
DIliushchenko <iliushchenkodmitrii@gmail.com>
2023-03-08 20:34:55 +0900

    updated list of species to align

--- scripts/separate_fasta.py
+++ scripts/separate_fasta.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 
 taxa_df = pd.read_csv('../data/taxa_gb.csv', sep='\t')
+obs_mut = pd.read_csv('../data/ObsMutSpec.csv')
+obs_sps = list(obs_mut['Species'])
 
 taxa_names: Set[str] = {'Actinopterygii', 'Amphibia', 'Lepidosauria', 'Mammalia', 'Aves'}
 
@@ -12,7 +14,7 @@ files_to_write_nc = {taxa: open(f'../data/CytB_ClassesNC/cytb_{taxa}.fasta', 'w'
 with open('../data/fasta/CYTB.fasta', 'r') as fasta_input:
     for rec in SeqIO.parse(fasta_input, 'fasta'):
         name = rec.description.replace(' ', '_')
-        if name in taxa_df['Species'].values:
+        if name in taxa_df['Species'].values and name in obs_sps:
             ind_taxa = taxa_df[taxa_df['Species'].isin([name])].index[0]
             class_in_taxa = taxa_df.iloc[ind_taxa, 0]
             header = '>' + name
