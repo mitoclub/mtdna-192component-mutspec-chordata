0a131354767b76fe64cd002818370bce44ef4522
kpotoh <axepon@mail.ru>
2024-06-18 00:06:55 +0200

    fix spectra missing 4 species; redo decomp with sigpro ana;ysis; make it beautiful and automatic

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -5,11 +5,17 @@ from pymutspec.annotation import rev_comp
 from pymutspec.constants import possible_sbs192, possible_sbs12, possible_sbs96
 
 
-def calc_mutspec_class(df: pd.DataFrame, group_col="Class"):
+def _calc_mutspec_class_legacy(df: pd.DataFrame, group_col="Class"):
     ms_cls = df.groupby([group_col, 'Mut'])['RawMutSpec'].sum().reset_index()
     ms_cls["RawMutSpecSum"] = ms_cls.Class.map(ms_cls.groupby("Class").RawMutSpec.sum().to_dict())
     ms_cls['MutSpec'] = ms_cls.RawMutSpec / ms_cls.RawMutSpecSum
     ms_cls = ms_cls.drop(['RawMutSpec', 'RawMutSpecSum'], axis=1)
+    return ms_cls
+
+
+def calc_mutspec_class(df: pd.DataFrame, group_col="Class"):
+    assert not df.MutSpec.isna().any()
+    ms_cls = df.groupby([group_col, 'Mut'])['MutSpec'].mean().reset_index()
     return ms_cls
 
 
